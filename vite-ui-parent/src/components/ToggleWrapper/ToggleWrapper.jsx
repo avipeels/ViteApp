da71d3d5fdce03e07a3daa94b7d4af38b5ffbcdd
Avinash Peelukhana <avinashpsk@gmail.com>
2026-01-15 23:45:38 +0530

    fix the bg color

--- vite-ui-parent/src/components/ToggleWrapper/ToggleWrapper.jsx
+++ vite-ui-parent/src/components/ToggleWrapper/ToggleWrapper.jsx
@@ -1,14 +1,10 @@
-import {Toggle} from 'sapphire-ui-toggle';
+import { Toggle } from 'sapphire-ui-toggle';
 
 export default function ToggleWrapper() {
   return (
-    <div className="page">
-      <div className="card">
-        <div className="toggle-container">
-        <Toggle size="md"/>
-        <Toggle size="lg"/>
-        </div>
-      </div>
+    <div>
+      <Toggle size="md" />
+      <Toggle size="lg" />
     </div>
   )
 }
